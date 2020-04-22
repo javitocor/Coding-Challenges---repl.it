da8e3d4d41e4914e142d451f2ab198c4e7a585fe
javitocor <56809963+javitocor@users.noreply.github.com>
2020-04-22 05:51:48 +0200

    Solved sliding maximun and added model solution

--- slidingmaximun.rb
+++ slidingmaximun.rb
@@ -2,7 +2,7 @@ def sliding_maximum(k, array)
     maximum = []
     for i in 0..array.length - k do
         max = array[i]
-        for j in i...i + k do
+        for j in i + 1...i + k do
             if array[j] > max
                 max = array[j]
             end
@@ -12,6 +12,9 @@ def sliding_maximum(k, array)
     maximum
 end
 
+def sliding_maximum(k, array)
+    array.each_cons(k).map(&:max)
+  end
   
   sliding_maximum(3, [1, 3, 5, 7, 9, 2])
   # => [5, 7, 9, 9]
