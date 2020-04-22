c0c43f6dbb73d79d441e0937a832d777e7096d3c
javitocor <56809963+javitocor@users.noreply.github.com>
2020-04-22 09:35:54 +0200

    Solved transposition

---
+++ transposition.rb
@@ -0,0 +1,15 @@
+def transpose(string)
+    while string.include?('gn')
+      string.gsub!("gn", 'ng')
+    end
+    string
+end
+    
+puts transpose('he was searchign on Bign for signign kigns')
+# => he was searching on Bing for singing kings
+    
+puts transpose('rignadingdiggn!')
+# => ringadingdingg!
+    
+puts transpose('gngngnnggnngggnnn')
+# => nnnnnnnnngggggggg
