5c10190736c5a69ecade5dfc088dcc07c5ece855
javitocor <56809963+javitocor@users.noreply.github.com>
2020-05-05 18:26:04 +0200

    Solved towers of hanoi 1

---
+++ towersofhanoi1.rb
@@ -0,0 +1,17 @@
+ def move(starting, goal)
+    empty = [1, 2, 3]
+    empty.delete(starting)
+    empty.delete(goal)
+    free= empty[0]
+    steps = ""
+    steps = "#{starting}->#{free}"+" "
+    steps+= "#{starting}->#{goal}"+" "
+    steps+= "#{free}->#{goal}"
+    steps
+  end
+
+puts move(1, 3)
+# => 1->2 1->3 2->3
+
+puts move(2, 3)
+# => 2->1 2->3 1->3
