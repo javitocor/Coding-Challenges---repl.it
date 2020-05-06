239cfde7a80db10e8ea71c09c16cc429f692706b
javitocor <56809963+javitocor@users.noreply.github.com>
2020-05-06 13:00:43 +0200

    Solved binary tree

--- binarytrees.rb
+++ binarytrees.rb
@@ -1,7 +1,7 @@
 def leftmost_nodes_sum(array)
     index = 0 
     sum = 0
-    while index < array.length
+    while index <= array.length
       sum += array[index]
       index = 2*index + 1
     end
