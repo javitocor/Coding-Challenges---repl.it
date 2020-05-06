e9bf95d21d13193b4a07fd24f794a8957ae62713
javitocor <56809963+javitocor@users.noreply.github.com>
2020-05-06 06:24:18 +0200

    Solved towers of hanoi 2

--- towersofhanoi2.rb
+++ towersofhanoi2.rb
@@ -3,9 +3,16 @@ def hanoi_steps(number_of_discs)
     starting = 1
     free = 2
     goal = 3
+    move(n, starting, goal, free)
+end
+
+def move(n, starting, goal, free)
     if n == 1
-        return 
+        puts "#{starting}->#{goal}"
     else
+        move(n-1, starting, free, goal)
+        move(1, starting, goal, free)
+        move(n-1, free, goal, starting)
     end
 end
   
