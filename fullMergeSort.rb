ff6b992d527b583b9f4ad48db2760284cbb9e49b
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-17 17:54:08 +0200

    solved full merge sort

--- fullMergeSort.rb
+++ fullMergeSort.rb
@@ -1,5 +1,27 @@
 def full_merge_sort(array)
+  mergesort(array).map { |x| x.split[1] }
+end
+
+def mergesort(array) 
+  return array if array.length <= 1 
   
+  pivot = array.length / 2 
+  
+  first = mergesort(array.slice(0, pivot))
+  last = mergesort(array.slice(pivot, array.length))
+  
+  merge(first, last)
+end 
+
+def merge(first, last)
+  return first if last.none?
+  return last if first.none?
+  
+  if first[0].split[0].to_i <= last[0].split[0].to_i
+    [first.first] + merge(first[1..-1], last)
+  else 
+    [last.first] + merge(first, last[1..-1])
+  end
   
 end
 
